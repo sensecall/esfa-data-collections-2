a5439cb459a8558f90f78d66f98aeada3a0431fd
Dan Sensecall <dan@sensecall.co.uk>
2018-02-15 22:12:15 +0000

    ILR routes

--- app/views/version-2/routes.js
+++ app/views/version-2/routes.js
@@ -16,9 +16,20 @@ router.get('/data-submissions', (req, res) => {
 	res.render(`${req.version}/data-submissions/index`,{currentPage})
 })
 
+// ILR
 router.get('/data-submissions/ilr', (req, res) => {
 	var currentPage = 'submit'
 	res.render(`${req.version}/data-submissions/ilr`,{currentPage})
+})
+
+router.get('/submit-your-ilr', (req, res) => {
+	var currentPage = 'submit'
+	res.render(`${req.version}/data-submissions/ilr/submit/index`,{currentPage})
+})
+
+router.get('/upload-your-ilr', (req, res) => {
+	var currentPage = 'submit'
+	res.render(`${req.version}/data-submissions/ilr/submit/upload-ilr`,{currentPage})
 })
 
 router.get('/login', (req, res) => {
