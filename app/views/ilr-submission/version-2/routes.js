4fea8cb13b69c2c61240a83fd6ee23736f301d25
Dan Sensecall <dan@sensecall.co.uk>
2018-04-18 11:47:48 +0100

    Start page now google

--- app/views/ilr-submission/version-2/routes.js
+++ app/views/ilr-submission/version-2/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = new express.Router()
 
 router.get('/', (req, res) => {
-  res.redirect(`/${req.feature}/${req.sprint}/start`)
+  res.redirect(`/${req.feature}/${req.sprint}/google-start`)
 })
 
 router.post('/upload-ilr-file', (req, res) => {
