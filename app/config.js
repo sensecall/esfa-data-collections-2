259a20411fb0f1a1578b7b96f4bb1e97e0168975
Liz <work@kidsontalks.com>
2018-04-19 12:49:34 +0100

    Content Update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Submit data to the ESFA',
+  serviceName: 'Submit learner data',
 
   // Default port that prototype runs on
   port: '3000',
